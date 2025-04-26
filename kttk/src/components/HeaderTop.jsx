599a68b30b29d60dbb0385d7c9ee232d73fac8df
Tan Loi <nguyenloidt.052017@gmail.com>
2025-04-26 16:27:16 +0700

    cau 1

---
+++ kttk/src/components/HeaderTop.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HeaderTop = () => (
+    <div className="header-top">
+        <div>
+            <a href="#" className="header-link">E-OFFICE</a>
+            <a href="#" className="header-link">EMAIL</a>
+            <a href="#" className="header-link">THƯ VIỆN - THÔNG TIN</a>
+        </div>
+        <div className="language">
+            <a href="#" className="header-link">KẾT NỐI | LIÊN HỆ</a>
+            <img src="https://flagcdn.com/16x12/vn.png" alt="Vietnamese Flag" className="flag" />
+            <img src="https://flagcdn.com/16x12/gb.png" alt="UK Flag" className="flag" />
+        </div>
+    </div>
+);
+
+export default HeaderTop;
---
+++ kttk/src/components/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HeaderTop from './HeaderTop';
+
+
+export default function Index() {
+    return (
+        <>
+            <HeaderTop /> 
+        </>
+    );
+}
